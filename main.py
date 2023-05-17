2b08209424c169dfd66a13419bddbb04b7c4f8ec
APKiani <133164212+APKiani@users.noreply.github.com>
2023-05-17 22:34:57 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,119 @@
-import time
 import psutil
 import platform
-
-# processor type and number of cores
-print(f"processor type : {platform.processor()}")
-print(f"number of physical cores: {psutil.cpu_count(logical=False)}")
-print(f"number of logical cores: {psutil.cpu_count(logical=True)}")
-# processor current frequency
-print(f"current cpu frequency : {psutil.cpu_freq().current}")
+import GPUtil
+from tabulate import tabulate
 
 
-# note that this part of the code should be operated in terminal!
-def display_usage(cpu_usage, mem_usage, bars=50):
-    cpu_percent = (cpu_usage / 100.0)
-    cpu_bar = '$' * int(cpu_percent * bars) + '-' * (bars - int(cpu_percent * bars))
-    mem_percent = (mem_usage / 100.0)
-    mem_bar = '$' * int(mem_percent * bars) + '-' * (bars - int(mem_percent * bars))
-    # Apologizes for the dollar sign I don't have numpad for putting a rectangular!!
-    print(f"\rCPU usage: |{cpu_bar}|{cpu_usage:.2f}% ", end="")
-    print(f"MEM usage: |{mem_bar}|{mem_usage:.2f}% ", end="\r")
+def get_size(bytes, suffix = "B"):
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
 
 
-while True:
-    display_usage(psutil.cpu_percent(), psutil.virtual_memory().percent, 30)
-    time.sleep(0.5)  # you can change the periods
+# system information
+print("*"*30, "System Information", "*"*30)
+uname = platform.uname()
+print(f"System: {uname.system}")
+print(f"Node Name: {uname.node}")
+print(f"Release: {uname.release}")
+print(f"Version: {uname.version}")
+print(f"Machine: {uname.machine}")
+print(f"Processor: {uname.processor}")
+
+# CPU information
+print("*"*30, "CPU Info", "*"*30)
+# number of cores
+print("Physical cores:", psutil.cpu_count(logical=False))
+print("Total cores:", psutil.cpu_count(logical=True))
+# CPU frequencies
+cpufreq = psutil.cpu_freq()
+print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
+print(f"Min Frequency: {cpufreq.min:.2f}Mhz")
+print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
+# CPU usage
+print("CPU Usage Per Core:")
+for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
+    print(f"Core {i}: {percentage}%")
+print(f"Total CPU Usage: {psutil.cpu_percent()}%")
+
+# Memory Information
+print("*"*30, "Memory Information", "*"*30)
+# RAM details
+ram = psutil.virtual_memory()
+print(f"Total: {get_size(ram.total)}")
+print(f"Available: {get_size(ram.available)}")
+print(f"Used: {get_size(ram.used)}")
+print(f"Percentage: {ram.percent}%")
+
+# Disk Information
+print("*"*30, "Disk Information", "*"*30)
+print("Partitions and Usage:")
+# get all disk partitions
+partitions = psutil.disk_partitions()
+for partition in partitions:
+    print(f"*** Device: {partition.device} ***")
+    print(f"  Mount point: {partition.mountpoint}")
+    print(f"  File system type: {partition.fstype}")
+    try:
+        partition_usage = psutil.disk_usage(partition.mountpoint)
+    except PermissionError:
+        # this can be caught due to the disk that
+        # isn't ready
+        continue
+    print(f"  Total Size: {get_size(partition_usage.total)}")
+    print(f"  Used: {get_size(partition_usage.used)}")
+    print(f"  Free: {get_size(partition_usage.free)}")
+    print(f"  Percentage: {partition_usage.percent}%")
+
+# Network information
+print("*"*30, "Network Information", "*"*30)
+# virtual and physical network info
+if_addrs = psutil.net_if_addrs()
+for interface_name, interface_addresses in if_addrs.items():
+    for address in interface_addresses:
+        print(f"*** Interface: {interface_name} ***")
+        if str(address.family) == 'AddressFamily.AF_INET':
+            print(f"  IP Address: {address.address}")
+            print(f"  Netmask: {address.netmask}")
+            print(f"  Broadcast IP: {address.broadcast}")
+        elif str(address.family) == 'AddressFamily.AF_PACKET':
+            print(f"  MAC Address: {address.address}")
+            print(f"  Netmask: {address.netmask}")
+            print(f"  Broadcast MAC: {address.broadcast}")
+"""
+for gpu you need to install gputil & tabulate to 
+print GPU information in a tabular way
+
+"""
+# GPU info
+print("*"*60, "GPU Details", "*"*60)
+gpus = GPUtil.getGPUs()
+list_gpus = []
+for gpu in gpus:
+    # get the GPU id
+    gpu_id = gpu.id
+    # name of GPU
+    gpu_name = gpu.name
+    # get % percentage of GPU usage of that GPU
+    gpu_load = f"{gpu.load*100}%"
+    # get free memory in MB format
+    gpu_free_memory = f"{gpu.memoryFree}MB"
+    # get used memory
+    gpu_used_memory = f"{gpu.memoryUsed}MB"
+    # get total memory
+    gpu_total_memory = f"{gpu.memoryTotal}MB"
+    # get GPU temperature in Celsius
+    gpu_temperature = f"{gpu.temperature} °C"
+    gpu_uuid = gpu.uuid
+    list_gpus.append((
+        gpu_id, gpu_name, gpu_load, gpu_free_memory, gpu_used_memory,
+        gpu_total_memory, gpu_temperature, gpu_uuid
+    ))
+
+print(tabulate(list_gpus, headers=("id", "name", "load", "free memory", "used memory", "total memory",
+                                   "temperature", "uuid")))
+
+
+
